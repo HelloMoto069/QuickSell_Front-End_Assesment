d3b9b029e11ebf2478d20728a7fd00ebf2bd7bf9
ANURUDDH <hellomoto.1769@gmail.com>
2022-04-23 22:03:31 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import DisplayBlock from './components/DisplayBlock';
-import Hightlight from './components/Highlight';
 import './index.css';
 
 ReactDOM.render(
